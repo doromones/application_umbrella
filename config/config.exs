6edd60f2a3d88c17df7998301f0acb17a3534620
a.radushev <a.radushev@starladder.tv>
2020-01-02 18:49:42 +0200

    add admin web

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :admin_web,
+  ecto_repos: [AdminWeb.Repo],
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :admin_web, AdminWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "tAH0sSjaxvMWiYz5Z2b7Mq7zWV/kSI3uDc2soSGOLwW4nCAiTSXpHT2v7duNGdDj",
+  render_errors: [view: AdminWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AdminWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
