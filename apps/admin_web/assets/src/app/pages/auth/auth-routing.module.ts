1ab2ad526e40328b24c61d9b7b65ea2efb3f03a1
doromones <doromones@gmail.com>
2020-01-08 21:08:38 +0200

    add no auth guard

--- apps/admin_web/assets/src/app/pages/auth/auth-routing.module.ts
+++ apps/admin_web/assets/src/app/pages/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from "./auth.component";
+import { NoAuthGuard } from "./no-auth.guard";
 
 
 const routes: Routes = [
-  {path: 'auth', component: AuthComponent}
+  {
+    path: 'auth',
+    component: AuthComponent,
+    canActivate: [NoAuthGuard]
+  }
 ];
 
 @NgModule({
