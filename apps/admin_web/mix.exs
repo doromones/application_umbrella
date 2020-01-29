3869241efb4a34976a3b1b02f34cf8ed879180bd
a.radushev <a.radushev@starladder.tv>
2020-01-29 18:32:17 +0200

    .

--- apps/admin_web/mix.exs
+++ apps/admin_web/mix.exs
@@ -44,7 +44,8 @@ defmodule AdminWeb.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:core, in_umbrella: true}
     ]
   end
 
